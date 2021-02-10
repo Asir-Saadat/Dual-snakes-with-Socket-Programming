264fd1842d2d3cb851e3cb6b4fdb95d8cd17e920
Asir-Saadat <66594633+Asir-Saadat@users.noreply.github.com>
2021-02-10 16:58:19 +0600

    Update server.py

--- server.py
+++ server.py
@@ -17,13 +17,13 @@ print("Waiting for a connection, Server Started")
 
 def read_pos(str):
     str = str.split(",")
-    return int(str[0]), int(str[1]), int(str[2])
+    return int(str[0]), int(str[1]), int(str[2]), int(str[3]), int(str[4]), int(str[5]), int(str[6]), int(str[7]), int(str[8]), int(str[9]), int(str[10]),int(str[11]), int(str[12]), int(str[13]), int(str[14]), int(str[15])
 
 
 def make_pos(tup):
-    return str(tup[0]) + "," + str(tup[1])+ ","+str(tup[2])
+    return str(tup[0]) + "," + str(tup[1])+ ","+str(tup[2])+","+str(tup[3])+","+str(tup[4])+","+str(tup[5])+","+str(tup[6])+","+str(tup[7])+","+str(tup[8])+","+str(tup[9])+","+str(tup[10])+","+str(tup[11])+","+str(tup[12])+","+str(tup[13])+","+str(tup[14])+","+str(tup[15])
 
-pos = [(0,0, 0),(100,100, 1)]
+pos = [(0,0, 0, 1, 200, 200, 1, 7, 0, 0, 0, 300, 300, 0, 0, 0),(100,100, 1,1,200, 200, 1 , 7, 0, 0, 0, 300, 300, 0, 0, 0)]
 
 def threaded_client(conn, player):
     #   player=1
